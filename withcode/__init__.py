394a442d26ed3dfb770dafe656c251b181900008
Pete Dring <dringp@fulford.york.sch.uk>
2022-02-19 16:12:59 +0000

    Added setuptools info

--- withcode/__init__.py
+++ withcode/__init__.py
@@ -4,7 +4,7 @@ withcode
 Offline python module to match visualisation and sound features of create.withcode.uk
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = 'Pete Dring'
 
 import tkinter
